ff25e76cd72e4b8a8d9032e25d97c39a5a5e79f4
Nico <aeonAvis@gmail.com>
2026-06-29 04:54:03 +0330

    Add documentation using godoc syntax

--- ch10/main.go
+++ ch10/main.go
@@ -1,5 +1,10 @@
+// Package ch10 includes functions to do simple mathematical operations
 package ch10
 
+// Add takes two integers and returns their sum
+// Addition rules are defined in [mathsisfun addition guide]
+//
+// [mathsisfun addition guide]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
